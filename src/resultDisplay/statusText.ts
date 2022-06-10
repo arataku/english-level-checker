d5d6dbbca2d5f1c7664455ab80b03a5f162bc505
arataku <44110164+arataku@users.noreply.github.com>
2022-06-10 22:27:35 +0900

    setTimeout -> window.setTimeout

--- src/resultDisplay/statusText.ts
+++ src/resultDisplay/statusText.ts
@@ -11,38 +11,39 @@ export class StatusText {
 
   start() {
     this.startTime = new Date().getTime();
-    const t = setTimeout(() => {
-      this.content = 'Scanning...';
-      this.timeout = this.timeout.filter(v => v !== t);
+    const t = window.setTimeout(() => {
+      this.content = "Scanning...";
+      this.timeout = this.timeout.filter((v) => v !== t);
     }, 300);
     this.timeout.push(t);
   }
 
   finishScan(wordCount: number) {
-    const t = setTimeout(() => {
+    const t = window.setTimeout(() => {
       this.wordCount = wordCount;
       this.content = `0/${this.wordCount} Words...`;
-      this.timeout = this.timeout.filter(v => v !== t);
+      this.timeout = this.timeout.filter((v) => v !== t);
     }, 300);
     this.timeout.push(t);
   }
 
   processedWordCountRefresh(processed: number) {
-    if(this.startTime === null) return;
-    if(new Date().getTime() - this.startTime < 300) return;
+    if (this.startTime === null) return;
+    if (new Date().getTime() - this.startTime < 300) return;
     this.content = `${processed}/${this.wordCount} Words...`;
   }
 
   finish(wordCount: number) {
-    this.timeout = this.timeout.filter(v => {
+    this.timeout = this.timeout.filter((v) => {
       clearInterval(v);
       return false;
     });
-    if(this.startTime !== null) {
-      this.content = `Ready! ${wordCount} Words (${new Date().getTime() - this.startTime}ms)`;
+    if (this.startTime !== null) {
+      this.content = `Ready! ${wordCount} Words (${
+        new Date().getTime() - this.startTime
+      }ms)`;
     } else {
       this.content = `Ready! ${wordCount} Words`;
     }
   }
-
 }
